191625cafe71b47131716a1ad3e3f3b45b6b386d
Moraru <cristian.moraru@isa.utm.md>
2024-09-11 14:08:33 +0300

    Login process & security

---
+++ src/main/java/com/example/project/controller/UserController.java
@@ -0,0 +1,70 @@
+package com.example.project.controller;
+
+import com.example.project.dto.UserDto;
+import com.example.project.entity.User;
+import com.example.project.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @GetMapping("/register")
+    public String showRegisterForm(Model model) {
+
+        UserDto userDto = new UserDto();
+        model.addAttribute("user", userDto);
+        return "register";
+    }
+
+    @PostMapping("/register/save")
+    public String registration(@Valid @ModelAttribute("user") UserDto userDto, BindingResult result, Model model) {
+
+        User existingUser = userService.findByEmail(userDto.getEmail());
+
+        if (existingUser != null && existingUser.getEmail() != null && existingUser.getEmail().isEmpty()) {
+            result.rejectValue("email", null, "There is already an account registered with this email");
+        }
+        if (result.hasErrors()) {
+            model.addAttribute("user", userDto);
+            return "redirect:/register?error";
+        }
+        userService.saveUser(userDto);
+        return "redirect:/register?success";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+
+        UserDto userDto = new UserDto();
+        model.addAttribute("user", userDto);
+        return "login";
+    }
+
+    @PostMapping("/login/verification")
+    public String loginVerification(@Valid @ModelAttribute("user") UserDto userDto) {
+
+        User existingUser = userService.findByEmail(userDto.getEmail());
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+        if (existingUser != null && passwordEncoder.matches(userDto.getPassword(), existingUser.getPassword())) {
+            return "redirect:/index";
+        } else {
+            return "redirect:/login?error";
+        }
+    }
+}
---
+++ src/main/java/com/example/project/service/UserServiceImpl.java
@@ -0,0 +1,68 @@
+package com.example.project.service;
+
+import com.example.project.dto.UserDto;
+import com.example.project.entity.Role;
+import com.example.project.entity.User;
+import com.example.project.repository.RoleRepository;
+import com.example.project.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private RoleRepository roleRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl (UserRepository userRepository,
+                            RoleRepository roleRepository,
+                            PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void saveUser(UserDto userDto) {
+        User user = new User();
+        user.setFirstName(userDto.getFirstName());
+        user.setLastName(userDto.getLastName());
+        user.setEmail(userDto.getEmail());
+        user.setPassword(passwordEncoder.encode(userDto.getPassword()));
+
+        Role role = roleRepository.findByName("ROLE_USER");
+
+        user.setRoles(Arrays.asList(role));
+        userRepository.save(user);
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public List<UserDto> findAllUsers() {
+        List<User> users = userRepository.findAll();
+        return users.stream().map((user) -> mapToUserDto(user)).collect(Collectors.toList());
+    }
+
+    private UserDto mapToUserDto(User user) {
+        UserDto userDto = new UserDto();
+        userDto.setId(user.getId());
+        userDto.setFirstName(user.getFirstName());
+        userDto.setLastName(user.getLastName());
+        userDto.setEmail(user.getEmail());
+        userDto.setRoles(user.getRoles().stream().map(Role::getName).collect(Collectors.toList()));
+        return userDto;
+    }
+}
