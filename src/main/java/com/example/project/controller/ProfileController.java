5f1c30a4fef509af156a7fbd3a1c6b4d0ba69992
Grigore <scutelnic.grigore10@gmail.com>
2024-09-17 09:42:53 +0300

    Registration date to profile added and connected to User

--- src/main/java/com/example/project/controller/ProfileController.java
+++ src/main/java/com/example/project/controller/ProfileController.java
@@ -2,9 +2,8 @@ package com.example.project.controller;
 
 import com.example.project.entity.Profile;
 import com.example.project.service.ProfileService;
-import com.example.project.service.UserService;
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,12 +16,10 @@ public class ProfileController {
     @Autowired
     private ProfileService profileService;
 
-    @Autowired
-    private UserService userService;
-
     @GetMapping()
-    public String showProfile(Model model) {
-        model.addAttribute("profile", new Profile());
+    public String showProfile(Model model, Authentication auth) {
+        Profile profile = profileService.findByEmail(auth.getName());
+        model.addAttribute("profile", profile);
         return "profile";
     }
 
