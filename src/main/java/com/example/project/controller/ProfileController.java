b2ce1f251849449c5eee3d08b9dcb0cff3bedd8f
Grigore <scutelnic.grigore10@gmail.com>
2024-09-18 13:02:16 +0300

    Modificari la profil

--- src/main/java/com/example/project/controller/ProfileController.java
+++ src/main/java/com/example/project/controller/ProfileController.java
@@ -6,8 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequestMapping("/profile")
 @Controller
@@ -25,7 +25,17 @@ public class ProfileController {
 
 
     @GetMapping("/editProfile")
-    public String showEditProfile() {
+    public String showEditContactForm(Model model, Authentication auth) {
+        Profile profile = profileService.findByEmail(auth.getName());
+        model.addAttribute("profile", profile);
         return "editProfile";
+    }
+
+    @PostMapping("/updateProfile")
+    public String updateContact(@ModelAttribute("profile") Profile editedProfile,
+                                @RequestParam("imageFile") MultipartFile imageFile,
+                                Authentication auth) {
+        profileService.updateProfile(editedProfile, imageFile, auth);
+        return "redirect:/profile/editProfile";
     }
 }
--- src/main/java/com/example/project/service/ProfileServiceImpl.java
+++ src/main/java/com/example/project/service/ProfileServiceImpl.java
@@ -1,9 +1,15 @@
 package com.example.project.service;
 
 import com.example.project.entity.Profile;
+import com.example.project.exceptions.InternalServerErrorException;
 import com.example.project.repository.ProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Service
 public class ProfileServiceImpl implements ProfileService {
@@ -19,5 +25,32 @@ public class ProfileServiceImpl implements ProfileService {
     @Override
     public Profile getProfileById(Long id) {
         return profileRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void updateProfile(Profile editedProfile, MultipartFile imageFile, Authentication auth) {
+        try {
+            Profile profile = profileRepository.findByEmail(auth.getName());
+            profile.setFirstName(editedProfile.getFirstName());
+            profile.setLastName(editedProfile.getLastName());
+            profile.setEmail(editedProfile.getEmail());
+            profile.setPhone(editedProfile.getPhone());
+            profile.setAddress(editedProfile.getAddress());
+            profile.setLocation(editedProfile.getLocation());
+            profile.setDescription(editedProfile.getDescription());
+
+            if (!imageFile.isEmpty()) {
+                try {
+                    byte[] imageBytes = imageFile.getBytes();
+                    profile.setImage(imageBytes);
+                } catch (IOException e) {
+                    throw new InternalServerErrorException("Failed to save image", HttpStatus.INTERNAL_SERVER_ERROR);
+                }
+            }
+
+            profileRepository.save(profile);
+        } catch (Exception e) {
+            throw new InternalServerErrorException("An unexpected error occurred", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
